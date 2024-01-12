bf12d42e6b09c867f7f3d6826a351bb64e8782a3
Zhuolun Liu <zhuolun.liu@citrix.com>
2024-01-12 15:41:56 -0500

    Support citrix cloud gov environment

--- client/citrixdaas_configuration.go
+++ client/citrixdaas_configuration.go
@@ -2,12 +2,14 @@
 
 package citrixclient
 
-// AuthenticationConfiguration provides authentication settings for CC Athena / on-prem trust service
+// AuthenticationConfiguration provides authentication settings for CC Athena / on-premises trust service
 type AuthenticationConfiguration struct {
 	AuthUrl      string `json:"auth_url"`
 	ClientId     string `json:"client_id"`
 	ClientSecret string `json:"client_secret"`
 	OnPremises   bool   `json:"on_premises"`
+	ApiGateway   bool   `json:"api_gateway"`
+	IsGov        bool   `json:"is_gov"`
 }
 
 // ClientConfiguration provides Citrix DaaS customer context
